3247b548a8b33d9e6aa392f4f6f3006a0dfe5e2b
Pavol Rusnak <pavol@rusnak.io>
2020-11-08 21:00:09 +0100

    init gh-pages

---
+++ precache-manifest.6f87c2f31fbcb698ed4f6bc5a0cd73cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f5b9a5f221ef3ba50c6ca38b7e5676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff07d7a9da5bc2e5c12b",
+    "url": "/static/css/main.23e55897.chunk.css"
+  },
+  {
+    "revision": "edcdad6081a19e3eae7b",
+    "url": "/static/js/2.f8a15ec8.chunk.js"
+  },
+  {
+    "revision": "ff07d7a9da5bc2e5c12b",
+    "url": "/static/js/main.766856dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
