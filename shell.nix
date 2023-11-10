d2e3eff730a65fabd280878a5b6f9e88bd28933a
Pavol Rusnak <pavol@rusnak.io>
2023-11-10 15:44:23 +0100

    chore: update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-# the last successful build of nixos-20.09 (stable) as of 2020-10-11
+# the last successful build of nixos-23.05 (stable) as of 2023-11-10
 with import
   (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz";
-    sha256 = "1l2hq1n1jl2l64fdcpq3jrfphaz10sd1cpsax3xdya0xgsncgcsi";
+    url = "https://github.com/NixOS/nixpkgs/archive/41de143fda10e33be0f47eab2bfe08a50f234267.tar.gz";
+    sha256 = "1xd9k5nv7i42wabrbv51hbbl81llmgijzqlargag2c40hsx1s276";
   })
 { };
 
